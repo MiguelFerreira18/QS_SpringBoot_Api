26b9ff4496b448a7ad5b89962604ec6f4d7f34a9
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2022-11-22 14:52:17 +0000

    laboratório feito

--- src/main/java/com/example/test/demo/controller/LaboratorioController.java
+++ src/main/java/com/example/test/demo/controller/LaboratorioController.java
@@ -1,4 +1,30 @@
 package com.example.test.demo.controller;
 
+import com.example.test.demo.model.Laboratorio;
+import com.example.test.demo.service.LaboratorioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class LaboratorioController {
+
+    @Autowired
+    LaboratorioService laboratorioService;
+
+    public String createLaboratorio(@RequestBody Laboratorio lab) throws Exception {
+        return laboratorioService.saveLaboratorio(lab);
+    }
+    public List<Laboratorio> deleteLaboratorio() throws Exception {
+        return laboratorioService.getAllLabs();
+    }
+    public String updateLaboratorio(@RequestBody Laboratorio lab) throws Exception {
+        return laboratorioService.updateLab(lab);
+    }
+    public String deleteLaboratorio(@RequestBody Laboratorio lab) throws Exception {
+        return laboratorioService.deleteLab(lab);
+    }
+
 }
--- src/main/java/com/example/test/demo/service/LaboratorioService.java
+++ src/main/java/com/example/test/demo/service/LaboratorioService.java
@@ -1,11 +1,65 @@
 package com.example.test.demo.service;
 
+import com.example.test.demo.model.Docente;
+import com.example.test.demo.model.Laboratorio;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class LaboratorioService {
 
     private static final String COL_NAME = "laboratirio";
 
+    public String saveLaboratorio(Laboratorio laboratorio) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        /*ADICIONA UM NOVO MATERIAL*/
+        ApiFuture<WriteResult> colApiFuture = db.collection(COL_NAME).document().set(laboratorio);
+        return colApiFuture.get().getUpdateTime().toString();
+    }
+
+    public List<Laboratorio> getAllLabs() throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection(COL_NAME).get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        List<Laboratorio> docentes = new ArrayList<>();
+        if (documents.size() > 0) {
+            for (QueryDocumentSnapshot doc : documents) {
+                docentes.add(doc.toObject(Laboratorio.class));
+            }
+            return docentes;
+        }
+        return null;
+    }
+
+    public String deleteLab(Laboratorio laboratorio) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection(COL_NAME).whereEqualTo("laboratorioId", laboratorio.getLaboratorioId()).get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        if (documents.size() > 0) {
+            ApiFuture<WriteResult> writeResultApiFuture = db.collection(COL_NAME).document(documents.get(0).getId()).delete();
+            return writeResultApiFuture.get().getUpdateTime().toString();
+        }
+        return "laboratorio não encontrado";
+    }
+
+    public String updateLab(Laboratorio laboratorio) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection(COL_NAME).whereEqualTo("laboratorioId", laboratorio.getLaboratorioId()).get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        if (documents.size() > 0) {
+            ApiFuture<WriteResult> writeResultApiFuture = db.collection(COL_NAME).document(documents.get(0).getId()).set(laboratorio);
+            return writeResultApiFuture.get().getUpdateTime().toString();
+        }
+        return "laboratorio não encontrado";
+    }
 
 }
