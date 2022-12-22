75b620bbda78c31203ba32bd110f05ca3b228b6a
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2022-12-22 23:18:06 +0000

    condiguração executada em compound, as requests são as mesmas muda é a porta...

--- src/main/java/com/example/test/demo/config/FBInitialize.java
+++ src/main/java/com/example/test/demo/config/FBInitialize.java
@@ -1,22 +1,30 @@
 package com.example.test.demo.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.stereotype.Service;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.net.ServerSocket;
 
 @Service
 public class FBInitialize {
+    private final ServerProperties serverProperties;
 
+    public FBInitialize(ServerProperties serverProperties) {
+        this.serverProperties = serverProperties;
+    }
 
     @PostConstruct
     public void Fbinit() {
-        boolean isTesting = true;//*?ALTERAR ESTA PARTE AQUI PARA TROCAR A BASE DE DADOS OU PARA TESTE OU PARA PRODUÇÃO?*/
+        int port = serverProperties.getPort();
+        System.out.println(port + " port ESTA È A MINHA PORTA PAH");
 
-        if (isTesting) {
+        if (port== 8080) {
             try {
                 FileInputStream serviceAccount = new FileInputStream("serviceAccountKeyTesting.json");
 
