f20e61188002ff377c6a604b82f30d3c7cf6af01
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2022-12-23 21:34:47 +0000

    branch de testes

---
+++ src/test/java/com/example/test/demo/service/ComponentServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.test.demo.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ComponentServiceTest {
+
+
+}
