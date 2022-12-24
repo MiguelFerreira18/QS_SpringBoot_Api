12a097cc5bc8efb0ba111c42348d01ca68099c12
MiguelFerreira18 <miguel.ferreira15@zonmail.pt>
2022-12-24 11:59:02 +0000

    Template dos testes

---
+++ src/test/java/com/example/test/demo/service/PedidoServiceTest.java
@@ -0,0 +1,60 @@
+package com.example.test.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PedidoServiceTest {
+
+    @Test
+    void getAllPedidos() {
+    }
+
+    @Test
+    void getAllPedidosUtilizador() {
+    }
+
+    @Test
+    void createPedidoUtilizador() {
+    }
+
+    @Test
+    void updatePedidoUtilizador() {
+    }
+
+    @Test
+    void deletePedidoUtilizador() {
+    }
+
+    @Test
+    void getAllPedidosMaterial() {
+    }
+
+    @Test
+    void createPedidoMaterial() {
+    }
+
+    @Test
+    void updatePedidoMaterial() {
+    }
+
+    @Test
+    void deletePedidoMaterial() {
+    }
+
+    @Test
+    void getAllPedidosLaboratorio() {
+    }
+
+    @Test
+    void createPedidoLaboratorio() {
+    }
+
+    @Test
+    void updatePedidoLaboratorio() {
+    }
+
+    @Test
+    void deletePedidoLaboratorio() {
+    }
+}
